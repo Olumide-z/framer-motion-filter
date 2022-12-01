8e28470c969b8c41d270eaf787f50d79e410006c
Olumide-Z <ilesanmiolumide10@gmail.com>
2022-12-01 13:39:53 +0100

    first commit

---
+++ src/Filter.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useEffect } from "react";
+
+
+
+const Filter = ({ setActiveGenre, popular, activeGenre, setFiltered }) => {
+  useEffect(() => {
+    if (activeGenre === 0) {
+      setFiltered(popular);
+      return;
+    }
+    const filtered = popular.filter((movie) =>
+      movie.genre_ids.includes(activeGenre)
+    );
+    setFiltered(filtered);
+  }, [activeGenre]);
+
+  return (
+    <div className="filter-container">
+      <button
+        onClick={() => setActiveGenre(0)}
+        className={activeGenre === 0 ? "active" : ""}
+      >
+        All
+      </button>
+      <button
+        onClick={() => setActiveGenre(35)}
+        className={activeGenre === 35 ? "active" : ""}
+      >
+        Comedy
+      </button>
+      <button
+        onClick={() => setActiveGenre(28)}
+        className={activeGenre === 28 ? "active" : ""}
+      >
+        Action
+      </button>
+    </div>
+  );
+};
+
+export default Filter;
